db41628a3489d2b3c39a03f60d2f342f992272b8
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2020-12-26 17:56:52 -0300

    using a tico.c file to include the header
    I was using the core.c directly, now it uses a tico.c file just as
    others nelua-decl bindings on the examples.
    
    I also moved the running directory of gcc, it runs directly on the
    repository's root instead of entering on nelua-decl.

--- setup-dev.sh
+++ setup-dev.sh
@@ -2,6 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+echo "setup-dev: removing possible existing tinycoffee and nelua-decl"
+rm -rf tinycoffee/
+rm -rf nelua-decl/
+
 echo "setup-dev: setup nelua-decl"
 
 # clone nelua-decl
---
+++ tico.c
@@ -0,0 +1,1 @@
+#include <tico.h>
